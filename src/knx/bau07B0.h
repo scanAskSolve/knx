c96a98be06d917cdee81f942187114271213699c
JIM\b1043 <b10430230@outlook.com>
2023-10-11 22:27:45 +0800

    Revert "20231008 V1.3.4 Fix Bau07B0, 原先繼承BauSystemBDevice和ITpUartCallBacks，將ITpUartCallBacks斷連結"
    This reverts commit 4b53fcbf94e3b66362efa8fff306c3af910924c8.

--- src/knx/bau07B0.h
+++ src/knx/bau07B0.h
@@ -7,8 +7,8 @@
 #include "tpuart_data_link_layer.h"
 #include "cemi_server.h"
 #include "cemi_server_object.h"
-//class Bau07B0 : public BauSystemBDevice, public ITpUartCallBacks
-class Bau07B0 : public BauSystemBDevice
+
+class Bau07B0 : public BauSystemBDevice, public ITpUartCallBacks
 {
   public:
     Bau07B0(ArduinoPlatform& platform);
@@ -21,7 +21,7 @@ class Bau07B0 : public BauSystemBDevice
     InterfaceObject* getInterfaceObject(ObjectType objectType, uint8_t objectInstance);
 
     // For TP1 only
-    virtual bool isAckRequired(uint16_t address, bool isGrpAddr);
+    bool isAckRequired(uint16_t address, bool isGrpAddr) override;
 
   private:
     TpUartDataLinkLayer _dlLayer;
