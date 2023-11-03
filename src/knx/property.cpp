f94116f343401b67221df1faf6e4d0924a2f4765
EDA\alex1 <alex1997860616@gmail.com>
2023-11-04 01:15:43 +0800

    20231104 V9 Fix DataProperty，尚未修完 可成功編譯，但是ETS燒入有問題

--- src/knx/property.cpp
+++ src/knx/property.cpp
@@ -413,6 +413,20 @@ uint8_t* Property::DataProperty_save(uint8_t* buffer)
 
     return buffer;
 }
+const uint8_t* Property::DataProperty_data()
+{
+    return _data;
+}
+
+const uint8_t* Property::DataProperty_data(uint16_t elementIndex)
+{
+    if ((elementIndex == 0) || (elementIndex > _currentElements))
+        return nullptr;
+
+    elementIndex -= 1; // Starting from 0
+    uint16_t offset = elementIndex * ElementSize();
+    return _data + offset;
+}
 
 
 
