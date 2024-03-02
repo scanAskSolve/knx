4bf70d7f9744c27e70cba5366bcaf0c56ca47d3a
JIM\b1043 <b10430230@outlook.com>
2024-03-03 02:06:15 +0800

    add .c .h

--- src/knx/dptc.h
+++ src/knx/dptc.h
@@ -16,35 +16,13 @@ typedef struct
 
 Dptc Dptc_create(unsigned short mainGroup,
                  unsigned short subGroup,
-                 unsigned short index)
-{
-    Dptc dptc;
-    dptc.mainGroup = mainGroup;
-    dptc.subGroup = subGroup;
-    dptc.index = index;
-    return dptc;
-}
-Dptc Dptc_default()
-{
-    Dptc dptc;
-    dptc.mainGroup = 0;
-    dptc.subGroup = 0;
-    dptc.index = 0;
-    return dptc;
-}
-Dptc Dptc_copy(Dptc &&dptc)
-{
-    Dptc result_dptc;
-    result_dptc.mainGroup = dptc.mainGroup;
-    result_dptc.subGroup = dptc.subGroup;
-    result_dptc.index = dptc.index;
-    return result_dptc;
-}
+                 unsigned short index);
 
-int Dptc_equals(Dptc dptc1, Dptc dptc2)
-{
-    return (dptc1.index == dptc2.index) && (dptc1.subGroup == dptc2.subGroup) && (dptc1.mainGroup == dptc2.mainGroup);
-}
+Dptc Dptc_default();
+
+Dptc Dptc_copy(Dptc dptc);
+
+int Dptc_equals(Dptc dptc1, Dptc dptc2);
 #define DPT_Switch Dptc_create(1, 1, 0)
 #define DPT_Bool Dptc_create(1, 2, 0)
 #define DPT_Enable Dptc_create(1, 3, 0)
